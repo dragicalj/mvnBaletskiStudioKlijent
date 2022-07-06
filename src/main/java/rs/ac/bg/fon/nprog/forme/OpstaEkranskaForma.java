29fcb118af9ef7ca1659b6ced6be48a6b897e4a0
dragicalj <gagaljubisavljevic111@gmail.com>
2022-07-06 20:44:46 +0200

    Kreirana apstraktna klasa OpstaEkranskaForma koju ce sve forme nasledjivati

---
+++ src/main/java/rs/ac/bg/fon/nprog/forme/OpstaEkranskaForma.java
@@ -0,0 +1,11 @@
+package rs.ac.bg.fon.nprog.forme;
+
+import javax.swing.JFrame;
+
+import rs.ac.bg.fon.nprog.domen.ApstraktniDomenskiObjekat;
+
+public abstract class OpstaEkranskaForma extends JFrame {
+	
+    public abstract ApstraktniDomenskiObjekat kreirajObjekat();
+
+}
